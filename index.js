1200e6717397b189df4005b88bf79c6b36a846c6
Kenneth Kobina Essien Aidoo <essienkent@gmail.com>
2026-06-19 17:23:22 +0000

    Add function to find the longest word length
    Implemented a function to find the length of the longest word in a string, along with test cases to validate its functionality.

---
+++ index.js
@@ -0,0 +1,20 @@
+function findLongestWordLength(str) {
+  const words = str.split(" ");
+  let maxLength = 0;
+
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].length > maxLength) {
+      maxLength = words[i].length;
+    }
+  }
+
+  return maxLength;
+}
+
+// Test cases
+console.log(findLongestWordLength("The quick brown fox jumped over the lazy dog")); // 6
+console.log(findLongestWordLength("May the force be with you")); // 5
+console.log(findLongestWordLength("Google do a barrel roll")); // 6
+console.log(findLongestWordLength("Googling do a barrel roll")); // 8
+console.log(findLongestWordLength("What is the average airspeed velocity of an unladen swallow")); // 8
+console.log(findLongestWordLength("What if we try a super-long word such as otorhinolaryngology")); // 19
